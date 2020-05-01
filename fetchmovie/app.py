5880b47f359273820acef49e4233895d424842c0
lfp <lihex.go@gmail.com>
2020-05-01 16:54:57 +0800

    refactor: Chained colorful output

--- fetchmovie/app.py
+++ fetchmovie/app.py
@@ -6,6 +6,7 @@ __description__ = '''
 import urllib3
 import sys
 from pathlib import Path
+from sgr_ansi import Chain
 import sgr_ansi as echo
 import click
 from vto.core import copy_to_clipboard
@@ -18,28 +19,24 @@ from fetchmovie.sites import bbt
 
 
 def fmt_help(*args, show_more=True, opt_hint='[OPT] '):
-    desc = echo.g(args[0], show=False)
+    desc = echo.Bg(args[0], show=False)
     if show_more:
         if len(args) > 1:
             args = list(args[1:])
             args.insert(0, opt_hint)
-            usage = ''.join([echo.b(x, show=False) for x in args])
+            usage = ''.join([echo.Ic(x, show=False) for x in args])
             desc = '{}\n{}'.format(desc, usage)
     return desc
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help'], terminal_width=200))
-@click.option('--name', '-n', help=fmt_help('movie name', '-n <name>'))
+@click.option('--name', '-n', help=fmt_help('movie/actor name', '-n <name>'))
 @click.option('--site', '-s', default='BBT', help=fmt_help('site name', '-s <site_name>'))
 @click.option('--overwrite', '-w', is_flag=True, help=fmt_help('overwrite local cache', '-w'))
 @click.option('--display_img', '-img', is_flag=True, help=fmt_help('display with image', '-img'))
 def run(name, site, display_img, overwrite=False):
-    echo.with_paragraph()
-    echo.BIUg(f'[SEARCH] site')
-    echo.BIUkw(f' {site} ')
-    echo.BIUg(f'for ')
-    echo.BIUbw(f'"{name}"', end='\n')
-    echo.with_paragraph(on=False)
+    chain = Chain()
+    chain = chain.BIUg(f'[SEARCH] site').BIUkw(f'{site}').BIUg(f'for').BIUb(f'"{name}"')
 
     link = bbt.run_bbt(name, display_img, overwrite=overwrite)
     echo.IUb(link)
