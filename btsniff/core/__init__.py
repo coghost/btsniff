49eb3767b58e316feab9f34290255b7d2db78412
hex <lihex.go@gmail.com>
2020-05-05 23:19:13 +0800

    feat: support search by selenium

--- btsniff/core/__init__.py
+++ btsniff/core/__init__.py
@@ -1,7 +1,8 @@
-from ._chrome import get_page_by_chrome
+from ._chrome import get_page_by_chrome, search_by_chrome
 from ._parser import PageParser
 
 __all__ = [
     'PageParser',
     'get_page_by_chrome',
+    'search_by_chrome',
 ]
--- btsniff/core/_chrome.py
+++ btsniff/core/_chrome.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
+from aibot import AiBot
 
 
-def init_driver():
+def init_driver(headless=True):
     chrome_options = webdriver.ChromeOptions()
     # remove "chrome is being controlled by automated software"
     chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -9,9 +10,16 @@ def init_driver():
     prefs = {
         "profile.password_manager_enabled": False,
         "credentials_enable_service": False,
+        'profile.default_content_setting_values': {
+            'images': 2,
+            'notifications': 2,
+            'javascript': 0,
+            'geolocation': 0,
+        }
     }
     chrome_options.add_experimental_option('prefs', prefs)
-    chrome_options.add_argument('--headless')
+    if headless:
+        chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=chrome_options)
@@ -22,4 +30,17 @@ def get_page_by_chrome(url):
     driver = init_driver()
     driver.get(url)
     dat = driver.page_source
+    driver.quit()
+    return dat
+
+
+def search_by_chrome(url, params, headless=True):
+    driver = init_driver(headless)
+    aib = AiBot(driver)
+    aib.get(url)
+    selector, txt = params
+    aib.update_text(selector, txt + '\n')
+    aib.bot.switch_to_window(1)
+    dat = aib.driver.page_source
+    aib.driver.quit()
     return dat
